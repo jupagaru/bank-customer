5913ef46b8a260a334c6691e2a9cea55112ff436
juan.garcia <jupagaru@gmail.com>
2022-02-12 09:27:46 -0500

    Creación método debeConsultarTodosLosCustomer, se agrega sl4j

--- src/test/java/com/vobi/bank/repository/CustomerRepositoryIT.java
+++ src/test/java/com/vobi/bank/repository/CustomerRepositoryIT.java
@@ -2,6 +2,7 @@ package com.vobi.bank.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -14,8 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.vobi.bank.domain.Customer;
 import com.vobi.bank.domain.DocumentType;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
+@Slf4j
 class CustomerRepositoryIT {
 	
 	@Autowired
@@ -103,6 +107,22 @@ class CustomerRepositoryIT {
 		//Assert
 		
 		assertFalse(customerOptional.isPresent(), "No pudo borrar el customer");
+	}
+	
+	@Test
+	@Order(5)
+	void debeConsultarTodosLosCustomer() {
+		
+		//Arrage
+		List<Customer> customers = null; 
+		
+		//Act
+		customers = customerRepository.findAll();
+		customers.forEach(customer->log.info(customer.getName()));
+		
+		//Assert
+		assertFalse(customers.isEmpty(), "No recuperó información");
+		
 	}
 
 }
